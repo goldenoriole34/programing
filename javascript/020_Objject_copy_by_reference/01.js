953193399530f8fce1ee9da6dbf0f566053adfe3
goldenoriole34 <83339731+goldenoriole34@users.noreply.github.com>
2022-04-07 17:54:04 +0900

    Revert "Revert "4-1 ~ 4-4""
    This reverts commit 7439b49c5be4833af51e500b8775ea649c2c121b.

---
+++ javascript/020_Objject_copy_by_reference/01.js
@@ -0,0 +1,12 @@
+let message = "Hello!";
+let phrase = message;
+
+//변수엔 객체가 그대로 저장되는 것이 아니라, 객체가 저장되어있는 '메모리 주소’인 객체에 대한 '참조 값’이 저장됨
+
+let user = { name: 'John' };
+
+let admin = user;
+
+admin.name = 'Pete'; // 'admin' 참조 값에 의해 변경됨
+
+alert(user.name); // 'Pete'가 출력됨.
