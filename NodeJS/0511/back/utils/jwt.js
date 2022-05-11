8f8540e8fbaaab14afb0311a3c3ec6f72030f20b
goldenoriole34 <83339731+goldenoriole34@users.noreply.github.com>
2022-05-11 15:50:21 +0900

    login logic

--- NodeJS/0511/back/utils/jwt.js
+++ NodeJS/0511/back/utils/jwt.js
@@ -1,45 +1,87 @@
-const crypto = require('crypto-js')
+// const crypto = require('crypto-js')
+const crypto = require('crypto')
 
-//header 
-const header = {
-  "alg": "HS256",
-  "typ": "JWT"
+// //header 
+// const header = {
+//   "alg": "HS256",
+//   "typ": "JWT"
+// }
+// //payload
+// const payload = {
+//   "sub": "1234567890",
+//   "name": "John Doe",
+//   "iat": 1516239022
+// }
+
+
+// //디코딩
+// // const decodingHeader = JSON.parse( Buffer.from( encodingHeader, 'base64').toString() )
+// // console.log("decodingHeader : "+ decodingHeader)
+
+// const encodingHeader =  Buffer.from( JSON.stringify(header))
+//                               .toString('base64')
+//                               .replace('==','')
+//                               .replace(/[=]/g,'')//64진수(아스키)
+
+// console.log("인코딩-헤더 : "+ encodingHeader)
+
+
+
+// const encodingPayload = Buffer.from( JSON.stringify(payload))
+//                               .toString('base64')
+//                               .replace(/[=]/g,'')
+
+// console.log("인코딩-페이로드 : " + encodingPayload)
+
+
+
+// const salt = 'ingoo'//salt는 암호규칙을 한층 더 강화하는 역할을 함
+// const cryptoSigature = crypto.createHmac('sha256', salt) 
+//                       .update(`${encodingHeader}.${encodingPayload}`)
+//                       .digest('base64')
+//                       .replace(/[=]/g,'')
+
+// const encodingCryptoSigature =  Buffer.from(cryptoSigature)                                  //인코딩한 해쉬 값
+//                                       .toString('base64')
+//                                       .replace('==','')
+//                                       .replace('=','') 
+
+
+// // console.log( '단방향-sha256 : ' + sigature)
+// console.log('크립토인코딩-시그니처 : ' + encodingCryptoSigature)
+// console.log(`크립토 버전으로 한번에 : ${encodingHeader}.${encodingPayload}.${encodingCryptoSigature}`)
+
+
+const encoding = (value) => {
+  return  Buffer.from( JSON.stringify(value))
+                .toString('base64')
+                .replace(/[=]/g,'')
 }
-//payload
-const payload = {
-  "sub": "1234567890",
-  "name": "John Doe",
-  "iat": 1516239022
+
+//토큰을 만들어주는 함수
+const createToken = (state) => {
+  const salt = 'ingoo'
+  const header = {"alg": "HS256","typ": "JWT"}
+  const payload = {...state}
+
+  const encodingHeader = encoding(header)
+  const encodingPayload = encoding(payload)
+  const encodingSigature = crypto.createHmac('sha256', salt)
+                                  .update(`${encodingHeader}.${encodingPayload}`)
+                                  .digest('base64')
+                                  .replace(/[=]/g,'')
+  return `${encodingHeader}.${encodingPayload}.${encodingSigature}`
 }
 
-//Buffer.from('str')
-// console.log(Buffer.from( JSON.stringify(header)) ) //16진수
 
-// Buffer.from( JSON.stringify(header).toString('base64').replace('==','').replace('=','') )//64진수(아스키)
-
-//코드가 기니 아래로
-
-const encodingHeader = Buffer.from( JSON.stringify(header)).toString('base64').replace('==','').replace('=','') //64진수(아스키)
-console.log("encodingHeader : "+ encodingHeader)
-
-//디코딩
-// const decodingHeader = JSON.parse( Buffer.from( encodingHeader, 'base64').toString() )
-// console.log("decodingHeader : "+ decodingHeader)
+createToken( { name : 'ingoo', age : 33 } )
 
 
-const encodingPayload = Buffer.from( JSON.stringify(payload)).toString('base64').replace('==','').replace('=','')
-console.log("encodingPayload : " + encodingPayload)
+console.log(createToken({name : 'ingoo'}))
+console.log(createToken({name : 'web7722'}))
+console.log(createToken({userid : 'web7722'}))
 
 
+//jwt토큰 만드는 함수 구현완료!!
 
-//sha256
-crypto.SHA256(`${encodingHeader}.${encodingPayload}`) //해쉬값을 또 인코딩 해줘야함
-
-const sigature = crypto.SHA256(`${encodingHeader}.${encodingPayload}`).toString()
-
-const encodingsigature = Buffer.from( sigature ).toString('base64').replace('==','').replace('=','')
-
-console.log( 'sha256 : ' + sigature)
-console.log('encodingsigature' + encodingsigature)
-
-
+//
