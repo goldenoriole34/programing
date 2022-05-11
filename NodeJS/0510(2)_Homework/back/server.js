7f3bc3fa7e9a2e56ac791103c00f4315bc7de561
goldenoriole34 <83339731+goldenoriole34@users.noreply.github.com>
2022-05-11 09:22:33 +0900

    axios post cookie send

--- NodeJS/0510(2)_Homework/back/server.js
+++ NodeJS/0510(2)_Homework/back/server.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
-app.use(cors())
+app.use(express.urlencoded({ extended : true }))
+
+app.use(express.json())
+
+app.use(cors({
+  origin : 'http://localhost:3000',
+  credentials : true
+}))
+
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('HelloWorld')
+})
+
+app.get('/getCookie', (req, res) => {
+  res
+  // .cookie('name', req.body.name)
+  .send('getCookie의 get페이지입니다')
+  console.log("get cookies : " + req.cookie)
+})
+
+app.post('/getCookie', (req, res) => {
+  console.log("쿠키 : " + req.body.name)
+  res
+  .cookie('name', req.body.name)
+  .send('cookie came!')
+  console.log("post cookies :" + req.cookies)
 })
 
 app.listen(3001, () => {
--- NodeJS/0510(2)_Homework/front/src/pages/index.js
+++ NodeJS/0510(2)_Homework/front/src/pages/index.js
@@ -3,20 +3,23 @@ import axios from 'axios'
 const Index = () => {
 
   const handleClick = async () => {
-    const result = await axios.get('http://localhost:3001')
-    alert(result.data)
+    const result = await axios.post('http://localhost:3001/getCookie',{
+      name : "heidi3" },{
+      withCredentials:'true' }
+    ).then ( (res) => { console.log(res) }
+    ).catch( (err) => { console.log(err) }
+    )
   }
   
   return(
-    <button onClick={handleClick}>Back에 요청</button>
+    <button onClick={handleClick}>Back에 전달</button>
   )
 }
 
 export default Index
 
 // 과제!!!
-// handleClick 눌렀을 때 브라우저
-// 요청 post로 보내고, getCookie axios
+// handleClick 눌렀을 때 브라우저에서 요청 post로 보내고, getCookie axios
 // router middleware -> 쿠키파서로 쿠키 생성,
 // 응답을 줌
 // res header set-cookie 정보 뜨게
